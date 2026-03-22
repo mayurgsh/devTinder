23e35a9bf520207c947b90a269b39cd76e92c24b
Mayur <mayurgsh@gmail.com>
2026-03-23 04:40:01 +0530

    exploring all get ,put,deleted changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-const express=require('express')
-const app=express()
-// app.use("/",(req,res)=>{
-//   res.send("hello from dashboard")
-// })
-app.use('/test',(req,res)=>{
-res.send("Hello from Test added")
+const express = require("express");
+const app = express();
+
+
+app.get('/user',(req,res)=>{
+  res.send({firstname:"Mayur",lastName:"shinde"})
 })
-app.listen(3000,()=>{
-  console.log("listning to server 3000 succesfully....")
+
+app.post('/user',(req,res)=>{
+  res.send("data saved succesfully into database....")
 })
+
+app.delete('/user',(req,res)=>{
+  res.send("user data deleted succesfully from database..")
+})
+
+app.listen(3000, () => {
+  console.log("listning to server 3000 succesfully....");
+});
