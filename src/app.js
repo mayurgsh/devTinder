a910fbee088f23408413570564cafa785f90dae7
Mayur <mayurgsh@gmail.com>
2026-03-22 19:55:03 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const app=express()
+// app.use("/",(req,res)=>{
+//   res.send("hello from dashboard")
+// })
+app.use('/test',(req,res)=>{
+res.send("Hello from Test")
+})
+app.listen(3000,()=>{
+  console.log("listning to server 3000 succesfully....")
+})
