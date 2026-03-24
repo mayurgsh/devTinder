58709101f418ceea3e15dcf8242cd75700eb55ec
Mayur <mayurgsh@gmail.com>
2026-03-24 17:43:03 +0530

    inner route handler code

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
-const express=require('express')
-const app=express()
+const express = require("express");
+const app = express();
 
-app.get('/user',(req,res)=>{
+app.use("/user", (req, res,next) => {
+  console.log("response 1 called");
+  res.send("response 1");
+  next()
+},(req,res)=>{
+  console.log("response 2 called")
+  // res.send("response 2")  
+});
 
-  res.send({name:"mayur",address:"Akola"})
-})
-
-app.get('/user/:userId',(req,res)=>{
-  console.log(req.params)
-  res.send("please find here perticular user details here")
-})
-
-
-
-
-
-app.get('/home',(req,res)=>{
-  res.send("this is home page...")
-})
-app.listen(3000,()=>{
-  console.log("server running on port 3000")
-})
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
